a0a8702c76ad4ab6cd914cc93f7fe672448ad151
dokien <2251272705@e.tlu.edu.vn>
2024-05-15 10:00:00 +0700

    update code

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require('cors');
+const indexRoute = require("./routes/index");
+const cookieParser = require('cookie-parser');
+global.__basedir = __dirname;
+
+
+app.use(express.static('public'));
+
+// static webpack
+app.use(express.static(path.join(__dirname, '../dist')))
+
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json())
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use("/", indexRoute)
+
+// error handler middleware
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).send({
+    error: {
+      status: error.status || 500,
+      message: error.message || 'Internal Server Error',
+    },
+  });
+});
+
+module.exports = {
+  app
+}
---
+++ client/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  mode: 'development',
+  entry: "./src/index.js",
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, '../dist'),
+    publicPath: '/',
+    clean: true // xoa di nhung file thua
+  },
+  watch: true,// đây là watch mode
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.join(__dirname, '../dist'),
+    hot: true,
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.jsx', '.css']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+      title: 'custom template',
+      inject: true
+    }),
+
+
+  ]
+  ,
+  module: {
+    rules: [
+      {
+        test: /\.js|\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css|\.scss?$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpg|png|woff|woff2|eot|ttf|svg)$/,
+        use: 'file-loader?limit=100000',
+      },
+    ],
+  },
+}
